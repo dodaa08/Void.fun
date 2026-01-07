0a5e3b6ad9874877b25f20e16273eb833cb768c1
dodaa08 <dodaa08@users.noreply.github.com>
2026-01-08 02:37:24 +0530

    Light theme verify page

--- fe/next.config.ts
+++ fe/next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    reactCompiler: false,
-  },
   transpilePackages: ['@solana/wallet-adapter-wallets'],
 };
 
